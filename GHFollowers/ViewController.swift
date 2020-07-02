eb2e15465941a8d2b7d8443b73211d915c26e7f0
MrBombHead <mrcraigcornwell@gmail.com>
2020-07-02 08:16:08 +0400

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Craig Cornwell on 02/07/2020.
+//  Copyright © 2020 Craig Cornwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
